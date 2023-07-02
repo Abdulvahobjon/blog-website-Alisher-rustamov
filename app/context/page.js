32a480f63cd7c27d262a1ba48400870cda21f94f
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-07-02 11:58:13 +0500

    commid

---
+++ app/context/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import { createContext, useContext , Dispatch , SetStateAction , useState } from "react"
+
+// interface ContextProps{
+//   userId: string,
+//   setUserId: Dispatch<SetStateAction<string>>,
+//   data: DataType[],
+//   setData 
+  
+// }
